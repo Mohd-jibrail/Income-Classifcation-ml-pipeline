12f3d7eef0d4bc877c364e177d822d6dda568213
MohdJIbrail <mohdjibrail34@gmail.com>
2025-01-26 23:13:10 +0530

    search, getcount, length of linked list

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -3,22 +3,26 @@ import LinkedList.Node;
 
 public class LinkedList {
     public Node head;
+    public int length=0;
+    void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+        newNode.next=head;
+        head = newNode;
+        length++;
+    }
     void insertAtEnd(int data){
         Node newNode = new Node(data);
         if(head==null){
             head=newNode;
+            length++;
         }else {
             Node temp=head;
             while (temp.next!=null){
                 temp=temp.next;
             }
             temp.next=newNode;
+            length++;
         }
-    }
-    void insertAtBeginning(int data){
-        Node newNode = new Node(data);
-        newNode.next=head;
-        head = newNode;
     }
     public void deleteNode(int key) {
         Node temp = head,prev=null;
@@ -32,6 +36,19 @@ public class LinkedList {
         }
         if(temp ==null) return;
         prev.next = temp.next;
+        length--;
+    }
+    int search(int key){
+        Node temp=head;
+        int index=0;
+        while (temp!=null){
+            if(temp.data==key){
+                return index;
+            }
+            temp=temp.next;
+            index++;
+        }
+        return -1;
     }
     void display(){
         Node temp=head;
@@ -39,6 +56,26 @@ public class LinkedList {
             System.out.print(temp.data+"->");
             temp=temp.next;
         }
+        System.out.println("null");
+    }
+    int getLength(){
+        return length;
+    }
+    int getCount(){
+        Node temp=head;
+        int length=0;
+        while (temp!=null){
+            length++;
+            temp=temp.next;
+        }
+        return length;
+    }
+    void reverseLinkedList(){
+        Node temp=head;
+        while (temp!=null){
+            temp=temp.next;
+        }
+        head=temp;
     }
     public static void main(String[] args){
         LinkedList list= new LinkedList();
@@ -46,8 +83,15 @@ public class LinkedList {
         list.insertAtEnd(23);
         list.insertAtEnd(25);
         list.insertAtBeginning(11);
-        list.deleteNode(12);
-        list.deleteNode(23);
+        list.insertAtBeginning(10);
+        list.insertAtEnd(27);
+        list.insertAtEnd(29);
+        System.out.println("Key 25 is at index :: "+list.search(25));
+        System.out.println("Linked List length is :: "+list.getLength());
+        System.out.println("Linked List length is :: "+list.getCount());
+        //Reversing the linked list now
+        list.display();
+        list.reverseLinkedList();
         list.display();
     }
 }
