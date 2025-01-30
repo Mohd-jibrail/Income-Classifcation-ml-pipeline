1c44dd0f0f72274fec1d1cf3859e0061d11b7528
MohdJIbrail <mohdjibrail34@gmail.com>
2025-01-30 16:05:07 +0530

    SMS Basic Project: Find student by roll number and Display single student node

--- src/SMS/SMSystem.java
+++ src/SMS/SMSystem.java
@@ -1,11 +1,11 @@
 package SMS;
 
 import java.util.Scanner;
-
+import SMS.Student;
 public class SMSystem {
     public Student student_start;
     public int studentSerialNumber=0;
-    String studentAdmission(String name, String father, String mother, String course, String code){
+    private String studentAdmission(String name, String father, String mother, String course, String code){
         Student newStudent= new Student(name, father,mother,course,code);
         if (student_start==null){
             student_start=newStudent;
@@ -21,7 +21,22 @@ public class SMSystem {
         }
         return "Student Added";
     }
-    void display(){
+
+    private Student getStudentInformation(String roll_number){
+        Student current_student=student_start;
+        if(current_student.getRoll_number().equalsIgnoreCase(roll_number)){
+            return current_student;
+        }
+        while (current_student!=null && current_student.next_student!=null){
+            if(current_student.getRoll_number().equalsIgnoreCase(roll_number)){
+                return current_student;
+            }else {
+                current_student=current_student.next_student;
+            }
+        }
+        return null;
+    }
+    private void display(){
         Student temp=student_start;
         while (temp!=null){
             System.out.println(temp.toString());
@@ -29,9 +44,18 @@ public class SMSystem {
         }
         System.out.println("null");
     }
+    public static String toStringStudent(Student student) {
+        return "Student{" +
+                "name='" + student.getName() + '\'' +
+                ", father_name='" + student.getFather_name()+ '\'' +
+                ", mother_name='" + student.getMother_name() + '\'' +
+                ", roll_number='" + student.getRoll_number() + '\'' +
+                ", course_name='" + student.getCourse_name() + '\'' +
+                ", course_code='" + student.getCourse_code() + "}";
+    }
     public static void main(String[] args){
         //System storage for students
-        SMSystem s=new SMSystem();
+        SMSystem smSystem=new SMSystem();
 
         Scanner sc= new Scanner(System.in);
         String add= "yes";
@@ -44,11 +68,17 @@ public class SMSystem {
             String mother_name =sc.nextLine();
             System.out.print("Course Name :: ");
             String course_name = sc.nextLine();
-            String status=s.studentAdmission( name,father_name,mother_name,course_name,"BCS");
+            String status=smSystem.studentAdmission( name,father_name,mother_name,course_name,"BCS");
             System.out.println("Admission Status :: "+status);
             System.out.println("Want to add more student type :: Yes/No");
             add=sc.nextLine();
         }
-        s.display();
+        smSystem.display();
+        System.out.println("Enter student roll number ::: ");
+        String roll_number =sc.nextLine();
+        Student student=smSystem.getStudentInformation(roll_number);
+        if(student!=null){
+            System.out.println(toStringStudent(student));
+        }
     }
 }
