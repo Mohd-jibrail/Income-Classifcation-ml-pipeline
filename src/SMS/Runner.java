76bc4ff54083450449c60ce534118d224077d5b2
MohdJIbrail <mohdjibrail34@gmail.com>
2025-01-30 23:23:22 +0530

    Implemented the singleton pattern for SMS project, cancelling the student admission

---
+++ src/SMS/Runner.java
@@ -0,0 +1,22 @@
+package SMS;
+
+import java.util.Scanner;
+import SMS.SMSystem.*;
+public class Runner {
+    public static void main(String[] args){
+        SMSystem activeStudents=SMSystem.getActiveStudentsList();
+        SMSystem deletedStudents=SMSystem.getDeletedStudents();
+        /* Scanner class for tacking the user input*/
+        Scanner sc= new Scanner(System.in);
+
+        /*-----------------------------*/
+        SMSystem.add(activeStudents, sc);
+        activeStudents.displayStudents();
+
+        /*-----------------------------*/
+        //SMSystem.displayStudent(activeStudents.getStudentInformation(sc.nextLine()));
+        System.out.println("Deleting the added student");
+        SMSystem.studentAdmissionCancel("DCS1");
+        deletedStudents.displayStudents();
+    }
+}
--- src/SMS/SMSystem.java
+++ src/SMS/SMSystem.java
@@ -1,11 +1,24 @@
 package SMS;
 
 import java.util.Scanner;
+
+import LinkedList.Node;
 import SMS.Student;
 public class SMSystem {
     public Student student_start;
-    public int studentSerialNumber=0;
-    private String studentAdmission(String name, String father, String mother, String course, String code){
+    public static int studentSerialNumber=0;
+    private SMSystem(){}
+    private static SMSystem activeStudents;
+    private static SMSystem deletedStudents;
+    public static SMSystem getActiveStudentsList(){
+        activeStudents= new SMSystem();
+        return activeStudents;
+    }
+    public static SMSystem getDeletedStudents(){
+        deletedStudents=new SMSystem();
+        return deletedStudents;
+    }
+    public String studentAdmission(String name, String father, String mother, String course, String code){
         Student newStudent= new Student(name, father,mother,course,code);
         if (student_start==null){
             student_start=newStudent;
@@ -21,8 +34,7 @@ public class SMSystem {
         }
         return "Student Added";
     }
-
-    private Student getStudentInformation(String roll_number){
+    public Student getStudentInformation(String roll_number){
         Student current_student=student_start;
         if(current_student.getRoll_number().equalsIgnoreCase(roll_number)){
             return current_student;
@@ -36,7 +48,7 @@ public class SMSystem {
         }
         return null;
     }
-    private void display(){
+    public void displayStudents(){
         Student temp=student_start;
         while (temp!=null){
             System.out.println(temp.toString());
@@ -44,20 +56,20 @@ public class SMSystem {
         }
         System.out.println("null");
     }
-    public static String toStringStudent(Student student) {
-        return "Student{" +
-                "name='" + student.getName() + '\'' +
-                ", father_name='" + student.getFather_name()+ '\'' +
-                ", mother_name='" + student.getMother_name() + '\'' +
-                ", roll_number='" + student.getRoll_number() + '\'' +
-                ", course_name='" + student.getCourse_name() + '\'' +
-                ", course_code='" + student.getCourse_code() + "}";
+    public static void displayStudent(Student student) {
+        String st=null;
+        if(student!=null){
+            st= "Student  {" +
+                    "name='" + student.getName() + '\'' +
+                    ", father_name='" + student.getFather_name()+ '\'' +
+                    ", mother_name='" + student.getMother_name() + '\'' +
+                    ", roll_number='" + student.getRoll_number() + '\'' +
+                    ", course_name='" + student.getCourse_name() + '\'' +
+                    ", course_code='" + student.getCourse_code() + "}";
+        }
+        System.out.println(st);
     }
-    public static void main(String[] args){
-        //System storage for students
-        SMSystem smSystem=new SMSystem();
-
-        Scanner sc= new Scanner(System.in);
+    public static void add(SMSystem smSystem, Scanner sc){
         String add= "yes";
         while (add.equalsIgnoreCase("yes")){
             System.out.print("Student Name :: ");
@@ -73,12 +85,25 @@ public class SMSystem {
             System.out.println("Want to add more student type :: Yes/No");
             add=sc.nextLine();
         }
-        smSystem.display();
-        System.out.println("Enter student roll number ::: ");
-        String roll_number =sc.nextLine();
-        Student student=smSystem.getStudentInformation(roll_number);
-        if(student!=null){
-            System.out.println(toStringStudent(student));
+    }
+    public static String studentAdmissionCancel(String roll_number){
+
+        Student temp_student=activeStudents.student_start;
+        Student prev_student=null;
+        if(temp_student!=null && temp_student.getRoll_number().equalsIgnoreCase(roll_number)){
+            activeStudents.student_start=temp_student.next_student;
+            return "Admission Cancelled";
         }
+        while (temp_student!= null && temp_student.next_student!=null){
+            if(temp_student.getRoll_number().equalsIgnoreCase(roll_number)){
+                deletedStudents.studentAdmission(temp_student.getName(),temp_student.getFather_name(),temp_student.getMother_name(),temp_student.getCourse_name(),temp_student.getCourse_code());
+                prev_student.next_student=temp_student.next_student;
+                studentSerialNumber--;
+
+            }
+            prev_student =temp_student;
+            temp_student=temp_student.next_student;
+        }
+        return "No Student Found";
     }
 }
